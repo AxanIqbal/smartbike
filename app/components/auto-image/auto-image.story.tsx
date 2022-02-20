f48cf0e83c52579189a1d821007349cf0ebc38e4
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-02-20 12:44:17 +0500

    ignite 7.10.2

--- app/components/auto-image/auto-image.story.tsx
+++ app/components/auto-image/auto-image.story.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import * as React from "react"
 import { storiesOf } from "@storybook/react-native"
-import { StoryScreen, Story, UseCase } from "../../../storybook/views"
+import { Story, StoryScreen, UseCase } from "../../../storybook/views"
 import { AutoImage } from "./auto-image"
 
 declare let module
@@ -14,11 +14,11 @@ storiesOf("AutoImage", module)
   .add("Style Presets", () => (
     <Story>
       <UseCase text="With require()">
-        <AutoImage source={bowser} />
-        <AutoImage source={bowser} style={{ width: 150 }} />
-        <AutoImage source={bowser} style={{ width: 150, height: 150 }} />
-        <AutoImage source={bowser} style={{ height: 150 }} />
-        <AutoImage source={bowser} style={{ height: 150, resizeMode: "contain" }} />
+        <AutoImage source={rick} />
+        <AutoImage source={rick} style={{ width: 150 }} />
+        <AutoImage source={rick} style={{ width: 150, height: 150 }} />
+        <AutoImage source={rick} style={{ height: 150 }} />
+        <AutoImage source={rick} style={{ height: 150, resizeMode: "contain" }} />
       </UseCase>
       <UseCase text="With URL">
         <AutoImage source={morty} />
